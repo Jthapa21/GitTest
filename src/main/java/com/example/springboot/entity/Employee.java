d02a34aceba5dcc3cb3d04e18b8a133c2bc3c1e5
jeny <jeny.thapa@outlook.com>
2023-02-18 20:37:12 -0600

    Backend

---
+++ src/main/java/com/example/springboot/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.example.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Table(name = "employees")
+@Data
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+
+
+
+
+}
